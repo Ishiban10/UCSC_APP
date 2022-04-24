1f0115ccadfb83439de26c456f6921308149e075
Ishaan Das <95513685+Ishiban10@users.noreply.github.com>
2022-04-24 16:59:33 -0700

    Add files via upload
    Second upload of Slug Central files

---
+++ MapsWebViews/WebViewPark.swift
@@ -0,0 +1,34 @@
+//
+//  WebViewPark.swift
+//  Slug Central
+//
+//  Created by Ishaan Das on 3/21/22.
+//
+
+import SwiftUI
+import WebKit
+
+
+struct WebViewPark: UIViewRepresentable {
+    
+        
+    
+    var url: String
+    
+    func makeUIView(context: Context) -> WKWebView {
+        
+        guard let url = URL(string: self.url) else {
+            return WKWebView()
+        }
+        
+        let request = URLRequest(url: url)
+        let wkWebView = WKWebView()
+        wkWebView.load(request)
+        return wkWebView
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: UIViewRepresentableContext<WebViewPark>) {
+        
+    }
+    
+}
---
+++ MapsWebViews/WebViewUpCamp.swift
@@ -0,0 +1,34 @@
+//
+//  WebViewUpCamp.swift
+//  Slug Central
+//
+//  Created by Ishaan Das on 3/21/22.
+//
+
+import SwiftUI
+import WebKit
+
+
+struct WebViewUpCamp: UIViewRepresentable {
+    
+        
+    
+    var url: String
+    
+    func makeUIView(context: Context) -> WKWebView {
+        
+        guard let url = URL(string: self.url) else {
+            return WKWebView()
+        }
+        
+        let request = URLRequest(url: url)
+        let wkWebView = WKWebView()
+        wkWebView.load(request)
+        return wkWebView
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: UIViewRepresentableContext<WebViewUpCamp>) {
+        
+    }
+    
+}
